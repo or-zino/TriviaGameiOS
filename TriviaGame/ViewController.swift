60f20b6296fa3a370d74425fa52ea6137ec6dcbc
orzino <orzino18@gmail.com>
2022-05-21 14:33:05 +0300

    Initial Commit

---
+++ TriviaGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriviaGame
+//
+//  Created by Or Zino on 21/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
